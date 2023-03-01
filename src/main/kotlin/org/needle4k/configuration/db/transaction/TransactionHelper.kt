1ea22d1c2f1961d856248e12ba73d2c9d842e9ea
Markus Dahm <markus.dahm@akquinet.de>
2023-03-01 09:20:04 +0100

    TransactionHelper

---
+++ src/main/kotlin/org/needle4k/configuration/db/transaction/TransactionHelper.kt
@@ -0,0 +1,124 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package org.needle4k.configuration.db.transaction
+
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.EntityManager
+
+/**
+ * Utility class to manage transactions conveniently.
+ *
+ * @author Heinz Wilming, Alphonse Bendt, Markus Dahm Akquinet AG
+ * @author Jan Galinski, Holisticon AG (jan.galinski@holisticon.de)
+ */
+@Suppress("MemberVisibilityCanBePrivate", "unused")
+class TransactionHelper(val entityManager: EntityManager) {
+
+  /**
+   * Saves the given object in the database.
+   *
+   * @param <T> type of given object obj
+   * @param obj object to save
+   * @return updated object
+   * @throws Exception save objects failed
+  </T> */
+  @Throws(Exception::class)
+  fun <T> saveObject(obj: T): T =
+    executeInTransaction { entityManager -> persist(obj, entityManager) }
+
+  /**
+   * Finds and returns the object of the given id in the persistence context.
+   *
+   * @param <T>   type of searched object
+   * @param clazz type of searched object
+   * @param id    technical id of searched object
+   * @return found object
+   * @throws Exception finding object failed
+  </T> */
+  @Throws(Exception::class)
+  fun <T> loadObject(clazz: Class<T>, id: Any): T =
+    executeInTransaction { entityManager -> loadObject(entityManager, clazz, id) }
+
+  /**
+   * Returns all objects of the given class in persistence context.
+   *
+   * @param <T>   type of searched objects
+   * @param clazz type of searched objects
+   * @return list of found objects
+   * @throws IllegalArgumentException if the instance is not an entity
+   * @throws Exception                loading objects failed
+  </T> */
+  @Throws(Exception::class)
+  fun <T> loadAllObjects(clazz: Class<T>): List<T> {
+    val entityAnnotation = clazz.getAnnotation(Entity::class.java)
+      ?: throw IllegalArgumentException("Unknown entity: " + clazz.name)
+
+    return executeInTransaction { entityManager ->
+      val fromEntity = entityAnnotation.name.ifEmpty { clazz.simpleName }
+
+      entityManager.createQuery("SELECT DISTINCT e FROM $fromEntity e")
+        .resultList as List<T>
+    }
+  }
+
+  /**
+   * Encapsulates execution of runnable.run() in transactions.
+   *
+   * @param <T>              result type of runnable.run()
+   * @param runnable         algorithm to execute
+   * @param clearAfterCommit <pre>true</pre> triggers entityManager.clear() after transaction
+   * commit
+   * @return return value of runnable.run()
+   * @throws Exception execution failed
+  </T> */
+  @Throws(Exception::class)
+  fun <T> executeInTransaction(runnable: Runnable<T>, clearAfterCommit: Boolean): T {
+    val result: T
+
+    try {
+      entityManager.transaction.begin()
+      result = runnable(entityManager)
+      entityManager.flush()
+      entityManager.transaction.commit()
+
+      if (clearAfterCommit) {
+        entityManager.clear()
+      }
+    } finally {
+      if (entityManager.transaction.isActive) {
+        entityManager.transaction.rollback()
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * see executeInTransaction(runnable, clearAfterCommit) .
+   *
+   * @param <T>      result type of runnable.run()
+   * @param runnable algorithm to execute
+   * @return return value of runnable.run()
+   * @throws Exception execution failed
+  </T> */
+  @Throws(Exception::class)
+  fun <T> executeInTransaction(runnable: Runnable<T>): T {
+    return executeInTransaction(runnable, true)
+  }
+
+  fun <T> persist(obj: T, entityManager: EntityManager): T {
+    entityManager.persist(obj)
+    return obj
+  }
+
+  fun <T> persist(obj: T): T {
+    return persist(obj, entityManager)
+  }
+
+  fun <T> loadObject(entityManager: EntityManager, clazz: Class<T>?, id: Any?): T {
+    return entityManager.find(clazz, id)
+  }
+}
+
+typealias Runnable<T> = (entityManager: EntityManager) -> T
--- src/test/kotlin/org/needle4k/reflection/ReflectionUtilTest.kt
+++ src/test/kotlin/org/needle4k/reflection/ReflectionUtilTest.kt
@@ -1,7 +1,10 @@
 package org.needle4k.reflection
 
 import jakarta.annotation.Resource
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.needle4k.MyComponentBean
 import org.needle4k.configuration.NeedleConfiguration
@@ -17,124 +20,115 @@ class ReflectionUtilTest {
   @Test
   fun testCanLookupPrivateFieldFromSuperclass() {
     val sample = DerivedClass()
-    val result: List<Field> = objectUnderTest.getAllFieldsWithAnnotation(sample, MyAnnotation::class.java)
-    Assert.assertEquals(result.size.toLong(), 1)
+    val result = objectUnderTest.getAllFieldsWithAnnotation(sample, MyAnnotation::class.java)
+
+    assertThat(result).hasSize(1)
   }
 
   @Test
   fun testSetFieldValue() {
     val sample = DerivedClass()
+
     objectUnderTest.setFieldValue(sample, "aPrivateField", "aValue")
-    Assert.assertEquals(sample.aPrivateField, "aValue")
+    assertEquals(sample.aPrivateField, "aValue")
+
     objectUnderTest.setFieldValue(sample, SampleClass::class.java, "aPrivateField", "otherValue")
-    Assert.assertEquals(sample.aPrivateField, "otherValue")
+    assertEquals(sample.aPrivateField, "otherValue")
   }
 
   @Test
   fun testGetAllFields() {
-    val allFields: List<Field> = objectUnderTest.getAllFields(DerivedClass::class.java)
-    Assert.assertEquals(allFields.size, 5)
+    assertThat(objectUnderTest.getAllFields(DerivedClass::class.java)).hasSize(5)
   }
 
   @Test
   fun testAllAnnotatedFields() {
-    val allAnnotatedFields: Map<Class<out Annotation?>, List<Field>> = objectUnderTest
-      .getAllAnnotatedFields(MyComponentBean::class.java)
-    Assert.assertEquals(4, allAnnotatedFields.size)
+    val allAnnotatedFields = objectUnderTest.getAllAnnotatedFields(MyComponentBean::class.java)
+    assertEquals(4, allAnnotatedFields.size)
+
     val list = allAnnotatedFields[Resource::class.java]!!
-    Assert.assertEquals(3, list.size)
+    assertEquals(3, list.size)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testInvokeMethod() {
     val invokeMethod = objectUnderTest.invokeMethod(this, "test") as String
-    Assert.assertEquals("Hello World", invokeMethod)
+
+    assertEquals("Hello World", invokeMethod)
   }
 
   @Test
   fun testGetFieldValue() {
     val address = Address()
+
     objectUnderTest.setField("id", address, 1L)
-    Assert.assertEquals(1L, objectUnderTest.getFieldValue(address, Address::class.java, "id"))
+    assertEquals(1L, objectUnderTest.getFieldValue(address, Address::class.java, "id"))
   }
 
   @Test(expected = IllegalArgumentException::class)
   fun testGetFieldValue_Exception() {
     val address = Address()
-    Assert.assertEquals(1L, objectUnderTest.getFieldValue(address, Address::class.java, "notexisting"))
+
+    assertEquals(1L, objectUnderTest.getFieldValue(address, Address::class.java, "notexisting"))
   }
 
   @Test(expected = NoSuchElementException::class)
   fun testGetField_NoSuchField() {
-    Assert.assertNull(objectUnderTest.getField(String::class.java, "fieldName"))
+    Assert.assertNull(objectUnderTest.getField(String::class.java, "notexisting"))
   }
 
   @Test
   fun testGetField_DerivedClass() {
-    Assert.assertNotNull(objectUnderTest.getField(DerivedClass::class.java, "aPrivateField"))
+    assertNotNull(objectUnderTest.getField(DerivedClass::class.java, "aPrivateField"))
   }
 
   @Test
-  @Throws(Exception::class)
   fun testGetMethodAndInvoke() {
-    val method: Method = objectUnderTest.getMethod(
+    val method = objectUnderTest.getMethod(
       DerivedClass::class.java, "testGetMethod", String::class.java, Int::class.javaPrimitiveType!!, Any::class.java
     )
-    Assert.assertNotNull(method)
-    val result: Any = objectUnderTest.invokeMethod(method, DerivedClass(), "Hello", 1, "")
-    Assert.assertEquals("Hello", result.toString())
+    assertNotNull(method)
+
+    val result = objectUnderTest.invokeMethod(method, DerivedClass(), "Hello", 1, "")
+    assertEquals("Hello", result.toString())
   }
 
   @Test
   fun testGetMethod() {
-    val methods: List<Method> = objectUnderTest.getMethods(
-      DerivedClass::class.java
-    )
-    Assert.assertEquals(13, methods.size)
+    assertThat(objectUnderTest.getMethods(DerivedClass::class.java)).hasSize(13)
   }
 
   @Test(expected = UnsupportedOperationException::class)
-  @Throws(Exception::class)
   fun testInvokeMethod_Exception() {
     objectUnderTest.invokeMethod(this, "testException")
   }
 
   @Test
   fun testGetAllFieldsAssignableFrom() {
-    val allFieldsAssignableFromBoolean = objectUnderTest.getAllFieldsAssignableFrom(
-      Boolean::class.java, DerivedClass::class.java
-    )
-    Assert.assertEquals(1, allFieldsAssignableFromBoolean.size)
-    val allFieldsAssignableFromList: List<Field> = objectUnderTest.getAllFieldsAssignableFrom(
-      MutableList::class.java, DerivedClass::class.java
-    )
-    Assert.assertEquals(2, allFieldsAssignableFromList.size)
-    val allFieldsAssignableFromCollection: List<Field> = objectUnderTest.getAllFieldsAssignableFrom(
-      MutableCollection::class.java, DerivedClass::class.java
-    )
-    Assert.assertEquals(2, allFieldsAssignableFromCollection.size)
-    val g: List<Field> = objectUnderTest.getAllFieldsAssignableFrom(
-      String::class.java, DerivedClass::class.java
-    )
-    Assert.assertEquals(2, g.size)
+    val allFieldsAssignableFromBoolean = objectUnderTest.getAllFieldsAssignableFrom(Boolean::class.java, DerivedClass::class.java)
+    assertEquals(1, allFieldsAssignableFromBoolean.size)
+    val allFieldsAssignableFromList = objectUnderTest.getAllFieldsAssignableFrom(List::class.java, DerivedClass::class.java)
+    assertEquals(2, allFieldsAssignableFromList.size)
+    val allFieldsAssignableFromCollection =
+      objectUnderTest.getAllFieldsAssignableFrom(Collection::class.java, DerivedClass::class.java)
+    assertEquals(2, allFieldsAssignableFromCollection.size)
+
+    val g: List<Field> = objectUnderTest.getAllFieldsAssignableFrom(String::class.java, DerivedClass::class.java)
+    assertEquals(2, g.size)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testCreateInstance() {
-    Assert.assertNotNull(objectUnderTest.createInstance(HashMap::class.java))
-    Assert.assertEquals("Hello", objectUnderTest.createInstance(String::class.java, "Hello"))
+    assertNotNull(objectUnderTest.createInstance(HashMap::class.java))
+    assertEquals("Hello", objectUnderTest.createInstance(String::class.java, "Hello"))
   }
 
   @Test(expected = Exception::class)
-  @Throws(Exception::class)
   fun testCreateInstance_Exception() {
     objectUnderTest.createInstance(InjectionTargetInformation::class.java)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testInvokeMethod_checkArgumentsWithPrimitives() {
     val derivedClass = DerivedClass()
     val intValue = 1
@@ -145,20 +139,21 @@ class ReflectionUtilTest {
     val byteValue: Byte = 2
     val shortValue: Short = 32
     val doubleValue = 24.1
-    val resultPrimatives: Any = objectUnderTest.invokeMethod(
+
+    val resultPrimitives = objectUnderTest.invokeMethod(
       derivedClass, "testInvokeWithPrimitive", intValue,
       floatValue, charValue, booleanValue, longValue, byteValue, shortValue, doubleValue
     )
-    Assert.assertEquals(true, resultPrimatives)
+    assertEquals(true, resultPrimitives)
+
     val resultObjects: Any = objectUnderTest.invokeMethod(
       derivedClass, "testInvokeWithObjects", intValue,
       floatValue, charValue, booleanValue, longValue, byteValue, shortValue, doubleValue
     )
-    Assert.assertEquals(true, resultObjects)
+    assertEquals(true, resultObjects)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testInvokeMethod_checkArgumentsWithObjects() {
     val derivedClass = DerivedClass()
     val intValue = 1
@@ -169,51 +164,44 @@ class ReflectionUtilTest {
     val byteValue: Byte = 2
     val shortValue: Short = 32
     val doubleValue = 24.1
-    val resultPrimitives: Any = objectUnderTest.invokeMethod(
+
+    val resultPrimitives = objectUnderTest.invokeMethod(
       derivedClass, "testInvokeWithPrimitive", intValue,
       floatValue, charValue, booleanValue, longValue, byteValue, shortValue, doubleValue
     )
-    Assert.assertEquals(true, resultPrimitives)
+    assertEquals(true, resultPrimitives)
+
     val resultObjects: Any = objectUnderTest.invokeMethod(
       derivedClass, "testInvokeWithObjects", intValue,
       floatValue, charValue, booleanValue, longValue, byteValue, shortValue, doubleValue
     )
-    Assert.assertEquals(true, resultObjects)
+    assertEquals(true, resultObjects)
   }
 
   @Test(expected = IllegalArgumentException::class)
-  @Throws(Exception::class)
   fun testInvokeMethod_NoSuchMethod() {
     val derivedClass = DerivedClass()
     objectUnderTest.invokeMethod(derivedClass, "testInvokeWithPrimitive")
   }
 
   @Test(expected = IllegalArgumentException::class)
-  @Throws(Exception::class)
   fun testInvokeMethod_WithWrongParameter() {
     objectUnderTest.invokeMethod(this, "test", 1.0)
   }
 
   @Test
   fun shouldFindAllMethodsWithMyAnnotation() {
-    val result: List<Method> = objectUnderTest.getAllMethodsWithAnnotation(
-      DerivedClass::class.java, MyAnnotation::class.java
-    )
-    Assert.assertEquals(result.size, 2)
+    val result: List<Method> = objectUnderTest.getAllMethodsWithAnnotation(DerivedClass::class.java, MyAnnotation::class.java)
+    assertEquals(result.size, 2)
   }
 
   @Suppress("unused")
-  private fun test(): String {
-    return "Hello World"
-  }
+  private fun test()= "Hello World"
+
+  @Suppress("unused", "UNUSED_PARAMETER")
+  private fun test(value: Int)=""
 
   @Suppress("unused")
-  private fun test(value: Int): String {
-    return ""
-  }
-
-  @Suppress("unused")
-  @Throws(UnsupportedOperationException::class)
   private fun testException() {
     throw UnsupportedOperationException()
   }
