f8e61863c09dc5a1f92e9eea7f8976525b63d3c1
Markus Dahm <markus.dahm@akquinet.de>
2023-03-16 13:27:16 +0100

    quickstarts

--- src/test/kotlin/org/needle4k/db/AbstractJPAEntityManagerTest.kt
+++ src/test/kotlin/org/needle4k/db/AbstractJPAEntityManagerTest.kt
@@ -1,7 +1,9 @@
 package org.needle4k.db
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert
+import org.junit.Assert.assertSame
 import org.mockito.Mockito
 import javax.inject.Inject
 import javax.persistence.EntityManager
@@ -11,7 +13,7 @@ abstract class AbstractJPAEntityManagerTest {
   protected lateinit var entityManager: EntityManager
 
   open fun `test with real entity manager`() {
-    Assertions.assertThat(Mockito.mockingDetails(entityManager).isMock).isFalse
+    assertThat(Mockito.mockingDetails(entityManager).isMock).isFalse
 
     val person = Person().apply { myName = "My Name" }
     val tx = entityManager.transaction
@@ -19,7 +21,7 @@ abstract class AbstractJPAEntityManagerTest {
     tx.begin()
     entityManager.persist(person)
     val fromDB = entityManager.find(Person::class.java, person.id)
-    Assert.assertSame(person, fromDB)
+    assertSame(person, fromDB)
     tx.commit()
   }
 
@@ -27,6 +29,7 @@ abstract class AbstractJPAEntityManagerTest {
     val myEntity = Person().apply { myName = "My Name" }
     val transactionHelper = TransactionHelper(entityManager)
     transactionHelper.saveObject(myEntity)
+
     val fromDb = transactionHelper.loadObject(Person::class.java, myEntity.id)
     Assert.assertFalse(myEntity === fromDb)
   }
